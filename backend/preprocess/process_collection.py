9bc72b4ad153aa11850799b359e2d198eb67dee2
joseporres <joseporres@users.noreply.github.com>
2021-07-29 22:36:34 -0500

    terminado, falta rangesearch y retocar

--- backend/preprocess/process_collection.py
+++ backend/preprocess/process_collection.py
@@ -2,7 +2,7 @@ import face_recognition
 from rtree import index 
 import os
 
-n = 100 #trabajaremos con 100 imagenes para ver el funcionamiento de nuestros algoritmos
+n = 100#trabajaremos con 100 imagenes para ver el funcionamiento de nuestros algoritmos
 
 def preprocess():    
     path = "lfw"
@@ -11,7 +11,7 @@ def preprocess():
     p.buffering_capacity = 10
     # p.dat_extension = 'data'
     # p.idx_extension = 'index'
-    Rtree = index.Index('Rtree', properties=p)
+    Rtree = index.Index('Rtree100', properties=p)
 
     listNames = os.listdir(path)
 
@@ -19,7 +19,7 @@ def preprocess():
     for name in listNames:
         imgList = os.listdir(path + "/" + name)
         for img in imgList: 
-            print(path + "/" + name + "/" + img)
+            #print(path + "/" + name + "/" + img)
             
             # obtenemos las caras
             imgPro = face_recognition.load_image_file(path + "/" + name + "/" + img)
@@ -32,14 +32,13 @@ def preprocess():
                 listAux = list(faceEncoding)
                 for cord in faceEncoding:
                     listAux.append(cord)
-                Rtree.insert(id, listAux, {"path": path + "/" + name, "name": img})
+                Rtree.insert(id, listAux, ("preprocess/" + path + "/" + name + "/" + img))
                 id += 1
 
             if id == n:
                 return Rtree
     Rtree.close()
 
-    print(str(id) + " images processed")
     return Rtree      
 
 preprocess()
